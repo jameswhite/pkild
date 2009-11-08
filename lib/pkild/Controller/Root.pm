1e5ed8ed78657ec56999790f7983dfaf9f35cba4
James White <whitejs@colbert.jameswhite.org>
2009-11-08 14:30:50 -0600

    dumping user to error

--- lib/pkild/Controller/Root.pm
+++ lib/pkild/Controller/Root.pm
@@ -49,7 +49,7 @@ sub default : Private {
         $c->stash->{template}="login.tt";
     }else{
         #$c->stash->{'ERROR'} = "Logged in as: ".$c->session->{'user'}->{ $c->session->{'user'}->{'user_field'} };
-        $c->stash->{'ERROR'} = "Logged in as: ".Data::Dumper->Dump([ $c->session->{'user'}->{'user_field'} ]);
+        $c->stash->{'ERROR'} = "Logged in as: ".Data::Dumper->Dump([ $c->session->{'user'} ]);
         $c->stash->{template}="application.tt";
     }
 }
