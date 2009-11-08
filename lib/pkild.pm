727b8db32939f0c385d6d56a3ef2fc69d3883107
James White <whitejs@colbert.jameswhite.org>
2009-11-08 11:37:53 -0600

    removing anonymous redirect hack

--- lib/pkild.pm
+++ lib/pkild.pm
@@ -38,8 +38,8 @@ __PACKAGE__->config(
 # Start the application
 __PACKAGE__->setup( qw/RequireSSL/ );
 __PACKAGE__->config->{require_ssl} = {
-        https => 'https://'.$c->request->{'server'},
-        http => 'http://'.$c-request->{'server'},
+        #https => 'https://'.$c->request->{'server'},
+        #http => 'http://'.$c-request->{'server'},
         remain_in_ssl => 1,
         no_cache => 1,
     };
