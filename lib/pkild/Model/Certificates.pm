987b8ea4226564cb3c7c2763d0c3851b568fa438
James White <whitejs@websages.com>
2009-12-14 09:59:34 -0600

    the host gpg key needs to be RSA

---
+++ lib/pkild/Model/Certificates.pm
@@ -0,0 +1,34 @@
+package pkild::Model::Certificates;
+
+use strict;
+use base 'Catalyst::Model::File';
+
+__PACKAGE__->config(
+    root_dir => '/var/lib/certificates',
+);
+
+=head1 NAME
+
+pkild::Model::Certificates - Catalyst File Model
+
+=head1 SYNOPSIS
+
+See L<pkild>
+
+=head1 DESCRIPTION
+
+L<Catalyst::Model::File> Model storing files under
+L<>
+
+=head1 AUTHOR
+
+James White
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
