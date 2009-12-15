18339e41685a1be820f03e72f33f26b4b98d2b30
James White <whitejs@websages.com>
2009-12-15 10:52:41 -0600

    this may asplode

--- lib/pkild/Controller/Root.pm
+++ lib/pkild/Controller/Root.pm
@@ -71,7 +71,8 @@ sub default : Private {
     ############################################################################
         if( $c->request->arguments->[0] eq "jstree" ){
             my $tree = $c->model('Certificates')->tree();
-            my $json_text = to_json( $tree );
+            my $children=$tree->{'children'};
+            my $json_text = to_json( $children );
             $c->res->body( $json_text );
         }
     }
