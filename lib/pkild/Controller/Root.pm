5054e2fc2cb2d097034de0c0020db1aa2be7e0b8
James White <whitejs@colbert.jameswhite.org>
2009-12-21 13:04:18 -0600

    open/close actions

--- lib/pkild/Controller/Root.pm
+++ lib/pkild/Controller/Root.pm
@@ -98,7 +98,7 @@ sub default : Private {
                     $c->res->body("[ ". join("\",\"",$c->session('opened_tabs'}. " ]");
                 }elsif($c->request->arguments->[2] eq "close" ){
                     # remove the tab node_id from the default open tabs
-                    while (my $item=shift @{ $session->{'opened_tabs'}){
+                    while (my $item=shift @{ $c->session->{'opened_tabs'}){
                         push(@{ $c->session->{'opened_tabs'} },$item) unless ($item eq $c->request->arguments->[3]);
                         $c->res->body("[ ". join("\",\"",$c->session('opened_tabs'}. " ]");
                     }
