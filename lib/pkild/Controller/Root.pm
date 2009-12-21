25d0f980550ecc20b98d1ade80002d7445673135
James White <whitejs@colbert.jameswhite.org>
2009-12-21 13:06:48 -0600

    open/close actions

--- lib/pkild/Controller/Root.pm
+++ lib/pkild/Controller/Root.pm
@@ -92,15 +92,15 @@ sub default : Private {
                                                            }
                                                          )
                                  );
-                }elsif($c->request->arguments->[2] eq "open" ){
-                    # add the tab node_id to the default open tabs
-                    push (@{ $c->session->{'opened_tabs'} },$c->request->arguments->[3]);
-                }elsif($c->request->arguments->[2] eq "close" ){
-                    # remove the tab node_id from the default open tabs
-                    while (my $item=shift @{ $c->session->{'opened_tabs'} }){
-                        push(@{ $c->session->{'opened_tabs'} },$item) unless ($item eq $c->request->arguments->[3]);
-                        $c->res->body("[ ". join("\",\"",$c->session{'opened_tabs'}). " ]");
-                    }
+#                }elsif($c->request->arguments->[2] eq "open" ){
+#                    # add the tab node_id to the default open tabs
+#                    push (@{ $c->session->{'opened_tabs'} },$c->request->arguments->[3]);
+#                }elsif($c->request->arguments->[2] eq "close" ){
+#                    # remove the tab node_id from the default open tabs
+#                    while (my $item=shift @{ $c->session->{'opened_tabs'} }){
+#                        push(@{ $c->session->{'opened_tabs'} },$item) unless ($item eq $c->request->arguments->[3]);
+#                        $c->res->body("[ ". join("\",\"",$c->session{'opened_tabs'}). " ]");
+#                    }
                 }
             }
         }
