1d86f1998b43210935c34dbf43f32d530f955a97
James S White <james@jameswhite.org>
2010-02-01 16:24:48 -0600

    switching to the tree select

--- lib/pkild/Controller/Root.pm
+++ lib/pkild/Controller/Root.pm
@@ -71,7 +71,7 @@ sub default : Private {
         }elsif( $c->request->arguments->[0] eq "action" ){
             # send the new actionbox
             if( $c->request->arguments->[1]){
-print STDERR "-=[ ".$c->request->arguments->[2]." ]=-\n";
+print STDERR "-=[ ".$c->request->arguments->[1]." ]=-\n";
                 if( $c->request->arguments->[1] eq "select" ){
 #                    $c->session->{'selected'} = $c->request->arguments->[2] if $c->request->arguments->[2];
                     ############################################################
