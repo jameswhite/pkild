5924165c5201a3a671be02250f5b736df8d2ab42
James S White <james@jameswhite.org>
2010-02-02 16:30:29 -0600

    jstree functionalized

--- lib/pkild/Controller/Root.pm
+++ lib/pkild/Controller/Root.pm
@@ -184,6 +184,7 @@ sub drawform : Local {
     # select the template from the template pool based on what
     # was selected and render it. 
     ############################################################
+    print STDERR Data::Dumper->Dump([$c->{'session'}->{menudata}]);
     $c->res->body( $c->view('TT')->render($c , 'form.tt', { 
                                                             additional_template_paths => [ $c->config->{root} . '/src'],
                                                             'form'=> $c->{'session'}->{menudata}->{'Domain'}
