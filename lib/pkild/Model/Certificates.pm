69813fc4881a524d05970a7a7c4b6d108e38fcde
James S White <james@jameswhite.org>
2010-02-02 13:18:09 -0600

    moving theme back to default

--- lib/pkild/Model/Certificates.pm
+++ lib/pkild/Model/Certificates.pm
@@ -57,18 +57,18 @@ sub tree{
             }
         }
     }
-    push(
-          @{ $tree->{''}->{'children'} }, 
-          { 
-            'attributes' => { 'id' =>  "NEW_ROOT_CA" }, 
-            'data' => { 'title' => 'Create New Root CA', 'icon' => 'createnew'},
-          }
-        );
     unshift(
           @{ $tree->{''}->{'children'} }, 
           { 
             'attributes' => { 'id' =>  "logout" }, 
             'data' => { 'title' => 'Logout', 'icon' => 'forbidden'},
+          }
+        );
+    push(
+          @{ $tree->{''}->{'children'} }, 
+          { 
+            'attributes' => { 'id' =>  "NEW_ROOT_CA" }, 
+            'data' => { 'title' => 'Create New Root CA', 'icon' => 'createnew'},
           }
         );
     return $tree->{''}->{'children'};
