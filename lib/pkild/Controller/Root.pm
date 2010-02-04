82f98d89bbfef954bf3275a3ab7b410d04c74953
James S White <james@jameswhite.org>
2010-02-04 16:45:12 -0600

    can update the crl path now

--- lib/pkild/Controller/Root.pm
+++ lib/pkild/Controller/Root.pm
@@ -97,8 +97,9 @@ sub default : Private {
                     # loop through the fields and set the value in the session.
                     if($c->request->arguments->[2]){
                         my @arg_list = @{$c->request->arguments};
-print STDERR "\n\n-=[".join("/",@arg_list)."]=-\n\n";
-                        my ($key,$value)=split(/=/,$c->request->arguments->[2]);
+                        shift @arg_list;
+                        shift @arg_list;
+                        my ($key,$value)=split(/=/,@arg_list);
                         for(my $idx=0;$idx<=$#{$c->session->{'menudata'}->{$c->session->{'current_node'}}->{'fields'}};$idx++){
                             if($c->session->{'menudata'}->{$c->session->{'current_node'} }->{'fields'}->[$idx]->{'name'} eq $key){
                                 $c->session->{'menudata'}->{ $c->session->{'current_node'} }->{'fields'}->[$idx]->{'value'}=$value;
