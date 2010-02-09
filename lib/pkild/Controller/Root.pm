e99123688c7aad454b30fa37bc690096e18940eb
James S White <james@jameswhite.org>
2010-02-09 10:57:53 -0600

    Making a 1:1 map for certs and logins

--- lib/pkild/Controller/Root.pm
+++ lib/pkild/Controller/Root.pm
@@ -258,7 +258,7 @@ sub drawform : Global {
                                                  )
                          );
         }else{
-my $entry=$c->session->{'user'}->{'user'}->{'ldap_entry'};
+my $entry=$c->session->{'user'}->{'user'}->{'ldap_entry'}->{'asn'};
 print STDERR Data::Dumper->Dump([$entry]);
             $c->res->body( $c->view('TT')->render($c , $c->session->{'menudata'}->{$menu}->{'template'}, { 
                                                                     additional_template_paths => [ $c->config->{root} . '/src'],
