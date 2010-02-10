216b40ea537caa8a83ceb0d3373787c76aca3382
James S White <james@jameswhite.org>
2010-02-10 12:24:57 -0600

    remove private dir on removal, it shouldn't exist, but just in case

--- lib/pkild/Model/Certificates.pm
+++ lib/pkild/Model/Certificates.pm
@@ -217,7 +217,7 @@ sub create_certificate{
 ### openssl ca -in $base/users/$1/$1.csr -cert $base/ca.crt -keyfile $base/ca.key -out $base/users/$1/$1.crt
         system("/usr/bin/openssl ca -config $certdata->{'config'} -policy policy_anything -out $certdata->{'certs'}/$objectname/$objectname.crt -batch -infiles $certdata->{'certs'}/$objectname/$objectname.csr");
         # convert to a pkcs12 container with the passphrase
-### openssl pkcs12 -export -clcerts -in $base/users/$1/$1.crt -inkey $base/users/$1/$1.key -out $base/users/$1/$1.p12
+        system("/bin/echo \"$param->{'password'}\" | /usr/bin/openssl pkcs12 -export -clcerts -passout fd:0 -in $certdata->{'certs'}/$objectname/$objectname.crt -inkey $certdata->{'certs'}/$objectname/private/$objectname.key -out $certdata->{'certs'}/$objectname/$objectname.p12");
         # read in the content fo the pkcs12 cert to memory
         # remove the pkcs12 cert from disk
         # return the content of the pkcs12 cert as a blob for file transfer to the client
