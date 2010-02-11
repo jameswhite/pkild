4cb3863b65b69ebb4947297839df5d09d52e303e
James S White <james@jameswhite.org>
2010-02-11 16:05:07 -0600

    restricting file views to certificate administrators

--- lib/pkild/Controller/Root.pm
+++ lib/pkild/Controller/Root.pm
@@ -60,7 +60,7 @@ sub default : Private {
     if(defined($c->req->param("get"))){ 
         if($c->req->param("get") eq "ca_trustchain"){
             $c->response->headers->header( 'content-type' => "application/x-x509-ca-cert" );
-            $c->response->headers->header( 'content-disposition' => "attachment; filename=".$c->model('Certificates')->object_domain($c->model('Certificates')->objectname($c->session->{'user'}))."crt" );
+            $c->response->headers->header( 'content-disposition' => "attachment; filename=".$c->model('Certificates')->object_domain($c->model('Certificates')->objectname($c->session->{'user'})).".crt" );
             $c->response->body($c->model('Certificates')->domain_trust_chain($c->model('Certificates')->object_domain($c->model('Certificates')->objectname($c->session->{'user'}))));
             $c->detach();
         }
