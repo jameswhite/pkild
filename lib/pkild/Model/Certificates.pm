2820c39cc76767c65e140d3fbf33b0dadd79babe
James S White <james@jameswhite.org>
2010-02-12 11:34:25 -0600

    adding host support

--- lib/pkild/Model/Certificates.pm
+++ lib/pkild/Model/Certificates.pm
@@ -338,6 +338,7 @@ sub sign_certificate{
     
     # convert the $self->{'node_separator'} delimited node names into a path
     my $node_dir = $self->actual_node($param->{'node_name'});
+print STDERR "\n=== $node_dir ===\n";
     $node_dir=~s/$self->{'node_separator'}/\//g;
     $node_dir=~s/certs$//g;
     $node_dir="$rootdir/$node_dir";
