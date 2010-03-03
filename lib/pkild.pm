a77df1672ac06aea24e37cd789e6e40f0732eb74
James S White <james@jameswhite.org>
2010-03-03 09:22:59 -0600

    un-bitching the config file

--- lib/pkild.pm
+++ lib/pkild.pm
@@ -31,7 +31,7 @@ our $VERSION = '0.01';
 
 __PACKAGE__->config( 
                      'name' => 'pkild',
-                     'authentication' => YAML::LoadFile( file(__PACKAGE__->config->{home}, 'Config.yaml')->{'authenticaton'}),
+                     'authentication' => YAML::LoadFile( file(__PACKAGE__->config->{home}, 'Config.yaml'))->{'authenticaton'},
                      'layout' => YAML::LoadFile( file(__PACKAGE__->config->{home}, 'root/forms/default.yaml')),
                    );
 
