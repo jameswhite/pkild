083a382ba6bf482e6de362f0867a02895ed31018
James White <whitejs@websages.com>
2010-07-24 16:28:24 -0500

    simplifying the user options

--- client/sign_csr.pl
+++ client/sign_csr.pl
@@ -25,7 +25,7 @@ while( ($successful_creation==0) && ($count < 6) ){
         # make our tmp dir
         my  $dir = tempdir( CLEANUP => 1 );
         # make our key
-        system("cd $dir; /usr/bin/openssl genrsa -out $host_long.key 1024");
+        system("cd $dir; /usr/bin/openssl genrsa -out $host_long.key");
         # get our openssl.cnf
         $mech->follow_link( 'text' => 'OpenSSL config for batch CSR creation' );
         open(OPENSSLCNF,">$dir/openssl.cnf");
