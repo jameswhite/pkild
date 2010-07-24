0b2f29d612110963614cd4ac447cae1fc889e1c7
James White <whitejs@websages.com>
2010-07-24 16:05:10 -0500

    simplifying the user options

--- lib/pkild/Model/Certificates.pm
+++ lib/pkild/Model/Certificates.pm
@@ -174,7 +174,7 @@ sub user_cert_exists{
 
 sub attr_for{
     my ($self,$session,$attr)=@_;
-    my $user_cert_dn=$self->user_cert_dn;
+    my $user_cert_dn=$self->user_cert_dn($session);
 print STDERR "$user_cert_dn :: $attr\n";
     my @subject_parts=split(",",$user_cert_dn);
     my $common_name;
