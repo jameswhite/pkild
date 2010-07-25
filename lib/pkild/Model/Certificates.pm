74b7f29dc75b40245236da6c6ae78543240ea8dc
James White <whitejs@websages.com>
2010-07-25 18:40:35 -0500

    building the tree

--- lib/pkild/Model/Certificates.pm
+++ lib/pkild/Model/Certificates.pm
@@ -781,12 +781,12 @@ sub ca_initialize{
     $tpldata->{'crl_path'}=~tr/A-Z/a-z/;
     $template->process(\$text,$tpldata,"$dir/openssl.cnf");
     # private.key
-    system("/usr/bin/openssl genrsa -out \"$dir/private/key\" 4096");
+    system("/usr/bin/openssl genrsa -out \"$dir/private/key\" 8192");
     # csr
-    system("/usr/bin/openssl req -new -sha1 -days 365 -key \"$dir/private/key\"  -out \"$dir/csr\" -config \"$dir/openssl.cnf\" -batch");
+    system("/usr/bin/openssl req -new -sha1 -days 3650 -key \"$dir/private/key\"  -out \"$dir/csr\" -config \"$dir/openssl.cnf\" -batch");
     # pem
     if(defined($parent)){
-        system("/usr/bin/openssl ca -extensions v3_ca -days 365 -out \"$dir/pem\" -in \"$dir/csr\" -config \"$parent/openssl.cnf\" -batch");
+        system("/usr/bin/openssl ca -extensions v3_ca -days 3650 -out \"$dir/pem\" -in \"$dir/csr\" -config \"$parent/openssl.cnf\" -batch");
     }else{
         system("openssl req -new -x509 -nodes -sha1 -days 3650 -key \"$dir/private/key\" -out \"$dir/pem\" -config \"$dir/openssl.cnf\" -batch");
 
