d16c2e8ce8516904cbe8fe8b351d2126ba1408ca
James White <whitejs@websages.com>
2010-07-31 16:31:02 -0500

    adding system exit status checks on revokation

--- lib/pkild/Model/Certificates.pm
+++ lib/pkild/Model/Certificates.pm
@@ -581,25 +581,25 @@ sub revoke_user_certificate{
     if( -f "$user_cert_file"){ print STDERR "Unable to remove $user_cert_file\n" };
     my $user_cert_dir=$self->user_cert_dir($session);
     my $pdir = $self->user_parent_cert_dir($session);
-    system("/usr/bin/openssl ca -revoke $user_cert_dir/crt -keyfile $pdir/private/key -cert $pdir/pem -config $pdir/openssl.cnf");
-    if($? == 0){
-        # update the Certificate Revocation list
-        system("/usr/bin/openssl ca -gencrl -keyfile $pdir/private/key -cert $pdir/pem -config $pdir/openssl.cnf -out $pdir/crl");
-        if($? == 0){
-            opendir(my $dh, "$user_cert_dir");
-            my @files = readdir($dh);
-            foreach my $file (@files){
-                unlink("$user_cert_dir/$file");
-            }
-            closedir $dh;
-            if( -d "$user_cert_dir"){ rmdir $user_cert_dir; };
-            if( -d "$user_cert_dir"){ print STDERR "Unable to remove $user_cert_dir\n" };
-        }else{
-            print STDERR "Unable to update the Certificate Revokation list\n";
-        }
-    }else{
-        print STDERR "Unable to revoke certificate.\n";
-    }
+#    system("/usr/bin/openssl ca -revoke $user_cert_dir/crt -keyfile $pdir/private/key -cert $pdir/pem -config $pdir/openssl.cnf");
+#    if($? == 0){
+#        # update the Certificate Revocation list
+#        system("/usr/bin/openssl ca -gencrl -keyfile $pdir/private/key -cert $pdir/pem -config $pdir/openssl.cnf -out $pdir/crl");
+#        if($? == 0){
+#            opendir(my $dh, "$user_cert_dir");
+#            my @files = readdir($dh);
+#            foreach my $file (@files){
+#                unlink("$user_cert_dir/$file");
+#            }
+#            closedir $dh;
+#            if( -d "$user_cert_dir"){ rmdir $user_cert_dir; };
+#            if( -d "$user_cert_dir"){ print STDERR "Unable to remove $user_cert_dir\n" };
+#        }else{
+#            print STDERR "Unable to update the Certificate Revokation list\n";
+#        }
+#    }else{
+#        print STDERR "Unable to revoke certificate.\n";
+#    }
     return $self;
 }
 
