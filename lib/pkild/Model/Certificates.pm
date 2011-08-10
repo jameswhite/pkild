e8354c3ff7e29ac7d77c9cbd83c2e3f796b04ee3
James S White <james@jameswhite.org>
2011-08-10 12:12:02 -0500

    pkcs12 fixes for the re-org

--- lib/pkild/Model/Certificates.pm
+++ lib/pkild/Model/Certificates.pm
@@ -256,10 +256,9 @@ sub objectname{
     my $user_session=shift;
     print STDERR Data::Dumper->Dump([ $user_session ]);
     print STDERR Data::Dumper->Dump([ $user_session->{'user'} ]);
-    print STDERR Data::Dumper->Dump([ $user_session->{'user'}->{'user'} ]);
-    print STDERR Data::Dumper->Dump([ $user_session->{'user'}->{'user'}->{'ldap_entry'} ]);
-    print STDERR Data::Dumper->Dump([ $user_session->{'user'}->{'user'}->{'ldap_entry'}->{'asn'} ]);
-    print STDERR Data::Dumper->Dump([ $user_session->{'user'}->{'user'}->{'ldap_entry'}->{'asn'}->{'objectName'} ]);
+    print STDERR Data::Dumper->Dump([ $user_session->{'user'}->{'ldap_entry'} ]);
+    print STDERR Data::Dumper->Dump([ $user_session->{'user'}->{'ldap_entry'}->{'asn'} ]);
+    print STDERR Data::Dumper->Dump([ $user_session->{'user'}->{'ldap_entry'}->{'asn'}->{'objectName'} ]);
     if(defined($user_session->{'user'}->{'user'}->{'ldap_entry'}->{'asn'}->{'objectName'})){
         print STDERR "exit objectname with objectname\n" if $self->{'trace'};
         return $user_session->{'user'}->{'user'}->{'ldap_entry'}->{'asn'}->{'objectName'};
