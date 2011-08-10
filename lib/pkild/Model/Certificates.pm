5318a9c499eef6ae063a88e4cefa06db5487b6ed
James S White <james@jameswhite.org>
2011-08-10 13:05:43 -0500

    pkcs12 fixes for the re-org

--- lib/pkild/Model/Certificates.pm
+++ lib/pkild/Model/Certificates.pm
@@ -1329,10 +1329,10 @@ sub node_type{
     ############################################################################     
     # print STDERR "[ $rootdir ] [ $node ] [ $nodepart[$#nodepart] ]\n";
     if(-d "$rootdir/$node"){ 
-        if($nodepart[$#nodepart] eq "cn=Root"){ return "ca";}
-        if($nodepart[$#nodepart] eq "cn=Intermediate"){ return "ca";}
-        if($nodepart[$#nodepart] eq "ou=People"){ return "certificate_authority";}
-        if($nodepart[$#nodepart] eq "ou=Hosts"){ return "certificate_authority";}
+        if($nodepart[$#nodepart] eq "cn=Root"){ return "certificate_authority";}
+        if($nodepart[$#nodepart] eq "cn=Intermediate"){ return "certificate_authority";}
+        if($nodepart[$#nodepart] eq "ou=People"){ return "certs";}
+        if($nodepart[$#nodepart] eq "ou=Hosts"){ return "certs";}
         if( ($nodepart[$#nodepart] =~ m/^cn=/) || ($nodepart[$#nodepart] =~ m/^uid=/)){ 
             if(-f "$rootdir/$node/$nodepart[$#nodepart].revoked"){
                 print STDERR "exit node_type revoked_certificate\n" if $self->{'trace'};
