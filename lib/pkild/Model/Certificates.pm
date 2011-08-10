93f818c895380391c1b09b58f5beedead0f5f6a8
James S White <james@jameswhite.org>
2011-08-09 20:52:16 -0500

    pkcs12 fixes for the re-org

--- lib/pkild/Model/Certificates.pm
+++ lib/pkild/Model/Certificates.pm
@@ -518,7 +518,7 @@ use File::Slurp;
     if($cn=~m/\s*uid=(.*)/){
         $type="user";
         $cn=~s/\s*uid=//;
-        $subject = $self->ca_based().", ou=People, cn=$cn/emailaddress=$cn\@$domain";
+        $subject = $self->ca_basedn().", ou=People, cn=$cn/emailaddress=$cn\@$domain";
     }
     if($cn=~m/\s*cn=(.*)/){
         $type="host";
