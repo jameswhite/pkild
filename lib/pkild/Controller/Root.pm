a01f9aaa7aa8298562eac8de9f3d8001eedcdb60
James S White <james@jameswhite.org>
2011-08-09 19:51:31 -0500

    pki structure re-org

--- lib/pkild/Controller/Root.pm
+++ lib/pkild/Controller/Root.pm
@@ -91,6 +91,11 @@ sub default : Private {
     $c->model('Certificates')->ca_basedn($c->config->{'global'}->{'cert_basedn'});
     $c->model('Certificates')->crl_base($c->config->{'global'}->{'crl_base'});
 
+    if( $c->check_user_roles( "certificate_administrators" ) ){ 
+        print STDERR "Admin: $c->user->{'user'}\n";
+    }else{
+        print STDERR "User: $c->user->{'user'}\n";
+    }
     ############################################################################
     # if we have no data to operate on, then forward to the "Create Tree" view
     ############################################################################
