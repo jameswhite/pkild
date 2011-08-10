176d6c9eaecddf494a9bfa861b97f6f276a03e0f
James S White <james@jameswhite.org>
2011-08-10 11:51:45 -0500

    pkcs12 fixes for the re-org

--- lib/pkild/Model/Certificates.pm
+++ lib/pkild/Model/Certificates.pm
@@ -458,8 +458,8 @@ sub opensslcnf_for{
 # /usr/bin/openssl genrsa -out \$(hostname -f).key 2048                         #
 #                                                                              #
 # Create your Certificate Signing Request with:                                #
-# /usr/bin/openssl req -new -sha1 -days 90 -key \$(hostname -f) \\              #
-#                      -out \$(hostname -f).csr -config openssl.cnf -batch       #
+# /usr/bin/openssl req -new -sha1 -days 90 -key \$(hostname -f) \\               #
+#                      -out \$(hostname -f).csr -config openssl.cnf -batch      #
 # (using this openssl.cnf)                                                     #
 #                                                                              #
 # Then paste the contents of the resulting *.csr into the textarea             # 
